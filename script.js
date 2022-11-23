95d580d263117c9fd9260fb6473688e6d11e194e
ashleydelic <ashleycohzer96@gmail.com>
2022-11-23 18:58:02 -0500

    seperate header component into its own js files

--- script.js
+++ script.js
@@ -1,18 +1,4 @@
 
-function Header() {
-    return (
-        <header>
-            <nav className="nav">
-            <img src="React-icon.png" />
-            <ul className="nav-items">
-                <li>Pricing</li>
-                <li>About</li>
-                <li>Contact</li>
-            </ul>
-            </nav>
-        </header>
-    )
-}
 
 function Page() {
     return (
