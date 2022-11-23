a293f60db3ccc5f393a321fbf60ac99261d8f323
ashleydelic <ashleycohzer96@gmail.com>
2022-11-23 13:29:52 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,19 @@
-const page = (
-    <div>
-        <img src="React-icon.png" width="40px" />
-        <h1>My awesome page in React</h1>
-        <h3>Reasons I love React</h3>
-        <ol>
-            <li>It's composable</li>
-            <li>It's declarative</li>
-            <li>It's hirable</li>
-            <li>It's actively maintained by skilled people</li>
-        </ol>
-    </div>
-)
 
-ReactDOM.render(page, document.getElementById("root"))
+
+function TemporaryName() {
+    return (
+        <div>
+            <img src="React-icon.png" width="40px" />
+            <h1>My awesome page in React</h1>
+            <h3>Reasons I love React</h3>
+            <ol>
+                <li>It's composable</li>
+                <li>It's declarative</li>
+                <li>It's hirable</li>
+                <li>It's actively maintained by skilled people</li>
+            </ol>
+        </div>
+    )
+}
+
+ReactDOM.render(<TemporaryName />, document.getElementById("root"))
