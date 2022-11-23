abb6f43e84f30decd67eb40bb8d36f80df25e8d3
ashleydelic <ashleycohzer96@gmail.com>
2022-11-23 10:30:43 -0500

    some html lists

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-ReactDOM.render(<h1>Hello everyone!</h1>, document.getElementById("root"))
+const page = (
+    <div>
+        <h1>My awesome page in React</h1>
+        <h3>Reasons I love React</h3>
+        <ol>
+            <li>It's composable</li>
+            <li>It's declarative</li>
+            <li>It's hirable</li>
+            <li>It's actively maintained by skilled people</li>
+        </ol>
+    </div>
+)
+
+ReactDOM.render(page, document.getElementById("root"))
