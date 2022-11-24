b1a0d14216aae1b5f01a413a8f751addad78781c
ashleydelic <ashleycohzer96@gmail.com>
2022-11-24 13:42:34 -0500

    main facts line height fix

--- Page.js
+++ Page.js
@@ -3,7 +3,7 @@ function Page() {
         <div className="main-content">
             <h1>My awesome page in React</h1>
             <h3>Reasons I love React</h3>
-            <ol>
+            <ol className="main-facts">
                 <li>It's composable</li>
                 <li>It's declarative</li>
                 <li>It's hirable</li>
