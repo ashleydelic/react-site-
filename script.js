e05368613f6c8b8edbe262e273128ba664633f1f
ashleydelic <ashleycohzer96@gmail.com>
2022-11-23 19:03:51 -0500

    page contents into its own js file

--- script.js
+++ script.js
@@ -1,19 +1,6 @@
 
 
-function Page() {
-    return (
-        <div>
-            <h1>My awesome page in React</h1>
-            <h3>Reasons I love React</h3>
-            <ol>
-                <li>It's composable</li>
-                <li>It's declarative</li>
-                <li>It's hirable</li>
-                <li>It's actively maintained by skilled people</li>
-            </ol>
-        </div>
-    )
-}
+
 
 function Footer() {
     return (
